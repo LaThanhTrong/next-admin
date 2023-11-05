b13652101e8e2be184fea837d0e37c71967c6e71
LaThanhTrong <lathanhtrong322002@gmail.com>
2023-11-05 13:29:19 +0700

    settings

--- pages/settings.js
+++ pages/settings.js
@@ -4,32 +4,32 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { withSwal } from "react-sweetalert2";
 import { useTheme } from "next-themes";
-function SettingsPage({swal}){
+function SettingsPage({ swal }) {
     const [inventory, setInventory] = useState([])
     const [featuredProductId, setFeaturedProductId] = useState('')
     const [isLoading, setIsLoading] = useState(false)
     const [shippingFee, setShippingFee] = useState('')
-    const {systemTheme, theme, setTheme} = useTheme()
+    const { systemTheme, theme, setTheme } = useTheme()
     const [mounted, setMounted] = useState(false)
-    
+
 
     const renderThemeChanger = () => {
-        if(!mounted){
+        if (!mounted) {
             return null
         }
         const currentTheme = theme === 'system' ? systemTheme : theme
 
-        if(currentTheme === 'dark'){
-            return(
+        if (currentTheme === 'dark') {
+            return (
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 cursor-pointer" onClick={() => setTheme('light')}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
                 </svg>
             )
         }
-        else{
-            return(
+        else {
+            return (
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 cursor-pointer" onClick={() => setTheme('dark')}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
                 </svg>
             )
         }
@@ -43,7 +43,7 @@ function SettingsPage({swal}){
         })
     }, [])
 
-    async function fetchAll(){
+    async function fetchAll() {
         await axios.get('/api/inventory').then(response => {
             setInventory(response.data)
         })
@@ -55,19 +55,19 @@ function SettingsPage({swal}){
         });
     }
 
-    async function saveSettings(){
-        if(shippingFee === ""){
+    async function saveSettings() {
+        if (shippingFee === "") {
             document.getElementById('shippingFee').style.display = "block"
         }
-        else{
+        else {
             setIsLoading(true)
-            await axios.put('/api/settings',{
+            await axios.put('/api/settings', {
                 name: 'featuredProductId',
                 value: featuredProductId,
             })
-            await axios.put('/api/settings',{
+            await axios.put('/api/settings', {
                 name: 'shippingFee',
-                value: shippingFee,   
+                value: shippingFee,
             })
             setIsLoading(false)
             await swal.fire({
@@ -77,7 +77,7 @@ function SettingsPage({swal}){
         }
     }
 
-    return(
+    return (
         <Layout>
             <h1 className="text-4xl font-bold mb-5">Settings</h1>
             {isLoading && (
@@ -102,9 +102,9 @@ function SettingsPage({swal}){
                         <div id="shippingFee" className="hidden text-red-500 mb-3">Shipping Price is required</div>
                         <div>
                             <button className="bg-[#4f46e5] p-2 px-4 rounded-lg text-white" onClick={saveSettings}>Save settings</button>
-                        </div> 
+                        </div>
                     </div>
-                    
+
                     <div className="flex items-center gap-4">
                         <h1 className="text-black dark:text-white font-bold text-xl">Toggle Light/Dark mode</h1>
                         {renderThemeChanger()}
@@ -115,6 +115,6 @@ function SettingsPage({swal}){
     )
 }
 
-export default withSwal(({swal}) => (
+export default withSwal(({ swal }) => (
     <SettingsPage swal={swal} />
-  ));
+));
